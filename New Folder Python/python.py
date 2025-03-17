b25d2585cc7125e8db87d254e3c394af6b98cc08
Austin Ash <A.Ash@albynschool.org>
2025-03-17 09:41:36 +0000

    fix

---
+++ New Folder Python/python.py
@@ -0,0 +1,9 @@
+print()
+the half time score for the hhomeHT = int(input("Enter ome team: "))
+awayHT = int(input("Enter the half time score for the away team: "))
+print()
+noGoalsScored = (homeFT + awayFT) - (homeHT + awayHT)
+homeFT = int(input("Enter the full time score for the home team: "))
+print("The number of goals scored after the half time break was: "+str(noGoalsScored))
+awayFT = int(input("Enter the full time score for the away team: "))
+
