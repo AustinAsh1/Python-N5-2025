524a43b33af58bc9209c66d30dd5059dc8bdf5ed
Austin Ash <A.Ash@albynschool.org>
2025-03-24 09:18:35 +0000

    hello sir

--- New Folder Python/308 task.py
+++ New Folder Python/308 task.py
@@ -7,7 +7,7 @@ while True:
 
     #Validate between 0 and 100
     while percentage < 0 or percentage > 100:
-    print("Error, % must be between 0 and 100")
+      print("Error, % must be between 0 and 100")
     percentage = int(input("Please enter a valid percentage"))
     # add score to list
 
---
+++ New Folder Python/task 3 309.py
@@ -0,0 +1,4 @@
+price = [3.99, 5.25, 10.50]
+total = 0
+for x in range(len(price)):
+  print('The total cost is: £' + str(total) )
