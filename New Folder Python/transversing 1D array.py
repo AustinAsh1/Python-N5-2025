2de7608544d742336889191ca3b326ae60b7430c
Austin Ash <A.Ash@albynschool.org>
2025-03-26 15:32:49 +0000

    jhiuhbj

---
+++ New Folder Python/transversing 1D array.py
@@ -0,0 +1,6 @@
+temperature = []
+input("Enter 7 daily tempratures between -20 and +50 C")
+while temperature <-20 or temperature > + 50:
+    Print ("these are invalid tempratures")
+    temp = int(input("please enter a valid temprature"))
+tempratures.append(temp)
