34a1db295e25b43951b9ade405935ccace7215ff
Austin Ash <A.Ash@albynschool.org>
2025-06-18 15:31:23 +0100

    your

---
+++ New Folder Python/314 example1.py
@@ -0,0 +1,19 @@
+#party cost
+childBuffet = 2.00
+if cakeRequired == "yes"
+   cake = 15.00
+else
+   cake = 0.00
+   noAdults = int(input(Enter number of adults))
+   noChildren = int (input(Enter number of children))
+for index in range (no children):
+   dietaryRequirements = input ("enter dietary requirements: ")
+   print(dietaryrequirements)
+if (noAdults + noChildren) > 20:
+   venue = 0.00
+else 
+   venue = 50.00
+cost = childBuffet * noChildren) + cake + venue
+print(cost)
+
+
